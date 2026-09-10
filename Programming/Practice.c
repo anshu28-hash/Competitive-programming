8a2c592f2f233ffd5fad67fc74b8ce12fd115e45
anshu28-hash <anshuman28092007@gmail.com>
2026-09-10 11:11:41 +0530

    sublime set up

--- Programming/Practice.c
+++ Programming/Practice.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 int main()
 {
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+
+    // Code part below this ::
+
+
     int x;
     scanf("%d", &x);
     // float y = ((x % 7 == 0) ? (x * 4.0) : (x / 3.0));
